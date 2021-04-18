83a2482fecd429e103c3584516d7d4b9a3e6f61d
Khalil Khassep <47596595+khalilKhassep@users.noreply.github.com>
2021-04-19 02:40:48 +0300

    Adding Add category dialog [Next:Enhance spaceing design of dialog

--- src/components/crud/category/Category.tsx
+++ src/components/crud/category/Category.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
+import Box from '@material-ui/core/Box';
 import ListCategories from './inc/ListCategories';
 import Button from '@material-ui/core/Button';
-
+import Dialog from './inc/Dialog';
 const Category = () => {
     //Fetch categories 
     const [categories, setCategories] = useState([]);
+    const [open , setOpen] = useState(false);
 
     useEffect(() => {
         fetchCategories(process.env.REACT_APP_BASE_URL, 'cat')
             .then(resolved => setCategories(resolved))
     }, []);
+
+    const handleOpen  = () => {
+        setOpen(true);
+        console.log(open)
+    };
 
     const fetchCategories = async (url: any, params: string) => {
         const _url: string = url + params;
@@ -21,12 +28,19 @@ const Category = () => {
         } else {
             return Promise.reject([request, 'There is an error']);
         }
-    }
+    };
+
+    const addCategory = () => {
+
+    };
 
     return (
         <>
         <ListCategories categories={categories}/>
-        <Button variant={'outlined'}> Add new category </Button>
+        <Box mt={4}>
+            <Button  onClick={handleOpen} variant={'outlined'}> Add  category </Button>
+        </Box>
+        <Dialog open={open} setOpen={setOpen}/>
         </>
 
     )
--- src/components/crud/category/inc/Dialog.tsx
+++ src/components/crud/category/inc/Dialog.tsx
@@ -1,7 +1,56 @@
-const Dialog  = () => {
-  return (
-      <p>Open dialog with action name and hndle action</p>
-  )
+import React, {useState, useEffect} from 'react';
+import MuiDialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+const Dialog = (props: any) => {
+    const [value, setValue] = useState('');
+
+    useEffect(() => {
+        // setOpen(props.open)
+    }, []);
+
+    const handleInput = (value: string) => {
+        setValue(value);
+        console.log(value);
+    };
+
+    const handleClose = () => {
+        props.setOpen(false);
+
+    };
+
+    return (
+        <MuiDialog open={props.open}>
+            <DialogTitle>{'Title'}</DialogTitle>
+            <DialogContent>
+                <form>
+                    <FormControl>
+                        <TextField
+                            variant={'outlined'}
+                            value={value} type={'string'}
+                            label={value}
+                            placeholder={value}
+                            onChange={event => {
+                                handleInput(event.target.value)
+                            }}
+                        />
+                    </FormControl>
+                    <ButtonGroup variant={'contained'} size={'small'} color={'primary'}>
+                        <Button> Save</Button>
+                        <Button onClick={handleClose}>Close</Button>
+                    </ButtonGroup>
+
+                </form>
+            </DialogContent>
+        </MuiDialog>
+    )
 };
 
 export default Dialog;
