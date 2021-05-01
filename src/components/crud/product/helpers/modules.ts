508869c534f3e1fdd9c4db0c964deaa453da7233
khalilKhassep <khalil.khassep@gmail.com>
2021-05-01 05:33:44 +0300

    Complete product form

--- src/components/crud/product/helpers/modules.ts
+++ src/components/crud/product/helpers/modules.ts
@@ -1,4 +1,10 @@
-import * as yup from 'yup'
-export const validationSchema = () => {
-  
-} 
+const getCategories = async (url: string) => {
+    const request = await fetch(url, {method: 'GET'});
+    if (request.ok) {
+        const response = await request.json();
+        return Promise.resolve(response.data)
+    }
+    return Promise.reject(false);
+};
+
+export {getCategories}
